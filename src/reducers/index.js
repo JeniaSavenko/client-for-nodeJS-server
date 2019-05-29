9ca323bba95515b6fcbf1dd4fdd5b0b4be746d7a
e.savenko <savenko.jenia@gmail.com>
2019-05-29 18:50:59 +0300

    -create PR

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,6 @@ import { postReducer } from './post';
 
 const rootReducer = combineReducers({
   post: postReducer
-
 });
 
 export default rootReducer;
--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,77 +1,77 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    ScrollView, View, FlatList, RefreshControl
+  ScrollView, FlatList, RefreshControl
 } from 'react-native';
-import {connect} from 'react-redux';
-import {Button} from 'react-native-elements';
-import {ListItem} from '../components/ListItem';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+import { ListItem } from '../components/ListItem';
 import Block from '../components/Block';
 import {
-    getPosts, rmPost, savePost, textEdit
+  getPosts, rmPost, savePost, textEdit
 } from '../actions/PostActions';
 
 
 const HomeScreen = ({
-                        posts,
-                        getPostsAction,
-                        navigation,
-                    }) => {
-    componentDidMount()
-    {
-        this.props.getPostsAction();
-    }
-    const [isLoading, setLoading] = useState(false);
+  posts,
+  getPostsAction,
+  navigation,
+}) => {
+  useEffect(() => {
+    getPostsAction();
+  }, []);
 
-    const update = async () => {
-        setLoading(false);
+  const [isLoading, setLoading] = useState(false);
 
-        posts = getPostsAction();
-    };
+  const update = async () => {
+    setLoading(false);
 
-    const renderItem = ({item}) => (
-        <ListItem
-            title={item.title}
-            text={item.text}
-            onPress={() => {
-                navigation.navigate('PostScreen', {
-                    post: item,
-                });
-            }}
-        />
-    );
+    posts = getPostsAction();
+  };
 
-    return (
-        <Block height width>
-            <ScrollView
-                refreshControl={(
-                    <RefreshControl
-                        refreshing={isLoading}
-                        onRefresh={update}
-                    />
+  const renderItem = ({ item }) => (
+    <ListItem
+      title={item.title}
+      text={item.text}
+      onPress={() => {
+        navigation.navigate('PostScreen', {
+          post: item,
+        });
+      }}
+    />
+  );
+
+  return (
+    <Block height width>
+      <ScrollView
+        refreshControl={(
+          <RefreshControl
+            refreshing={isLoading}
+            onRefresh={update}
+          />
                 )}
-            >
-                <FlatList
-                    data={posts}
-                    renderItem={renderItem}
-                    initialNumToRender={10}
-                    maxToRenderPerBatch={10}
-                />
-            </ScrollView>
-            <Button title="Add Posts" onPress={() => navigation.navigate('AddPost')}/>
-        </Block>
-    );
+      >
+        <FlatList
+          data={posts}
+          renderItem={renderItem}
+          initialNumToRender={10}
+          maxToRenderPerBatch={10}
+        />
+      </ScrollView>
+      <Button title="Add Posts" onPress={() => navigation.navigate('AddPost')} />
+    </Block>
+  );
 };
 
 
 const mapStateToProps = store => ({
-    posts: store.post.posts,
+  posts: store.post.posts,
 });
 
 const mapDispatchToProps = dispatch => ({
-    getPostsAction: () => dispatch(getPosts()),
-    deletePostAction: item => dispatch(rmPost(item)),
-    textEditAction: (itemId, itemValue) => dispatch(textEdit(itemId, itemValue)),
-    saveTextAction: (itemId, text, title) => dispatch(savePost(itemId, text, title))
+  getPostsAction: () => dispatch(getPosts()),
+  deletePostAction: item => dispatch(rmPost(item)),
+  textEditAction: (itemId, itemValue) => dispatch(textEdit(itemId, itemValue)),
+  saveTextAction: (itemId, text, title) => dispatch(savePost(itemId, text, title))
 
 });
 
